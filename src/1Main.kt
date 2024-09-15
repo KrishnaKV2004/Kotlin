1365b2816ab78824de8d140ac12a62fbce0635f3
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2024-09-15 17:58:19 +0530

    Added Kotlin Files

---
+++ src/1Main.kt
@@ -0,0 +1,7 @@
+//  Main Function -->
+
+fun main(args: Array<String>)   //  can be empty or command line arguments can be passed
+{
+    println("Hello World!")
+    println("Arguments --> ${args.joinToString()}")
+}
