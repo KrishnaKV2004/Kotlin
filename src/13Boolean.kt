cfdd70060add278f958acc9b5ad2959c71559d0b
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2024-09-17 10:59:26 +0530

    Boolean

---
+++ src/13Boolean.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    //  Possible values of Boolean Datatype -->
+
+    val isAdult = true
+    val isMale = false
+    val orderCompleted: Boolean? = null
+
+    println("Is Adult ? --> $isAdult")
+    println("Is Male ? --> $isMale")
+    println("Is Order Completed ? --> $orderCompleted")
+}
