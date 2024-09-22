d7c81a5a7a4205f4576128224e80e4ef9220e830
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2024-09-22 10:20:27 +0530

    Modified

--- src/33VisibilityTest.kt
+++ src/33VisibilityTest.kt
@@ -6,3 +6,12 @@ fun main()
     // println(example.protectedVar) // Error: protectedVar is protected
     println(example.internalVar) // Accessible within the same module
 }
+
+/*
+    Key Points -->
+
+    public is the default visibility modifier and allows access from anywhere.
+    private restricts access to the class or file in which it's declared.
+    protected allows access within the class and its subclasses.
+    internal allows access within the same module but not outside it.
+*/
