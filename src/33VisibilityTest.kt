835191015eacff5169c8b348670c0e3482f9f5e3
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2024-09-22 10:21:12 +0530

    Modified

--- src/33VisibilityTest.kt
+++ src/33VisibilityTest.kt
@@ -1,12 +1,3 @@
-fun main()
-{
-    val example = VisibilityExample()
-    println(example.publicVar)   // Accessible
-    // println(example.privateVar) // Error: privateVar is private
-    // println(example.protectedVar) // Error: protectedVar is protected
-    println(example.internalVar) // Accessible within the same module
-}
-
 /*
     Key Points -->
 
@@ -15,3 +6,12 @@ fun main()
     protected allows access within the class and its subclasses.
     internal allows access within the same module but not outside it.
 */
+
+fun main()
+{
+    val example = VisibilityExample()
+    println(example.publicVar)   // Accessible
+    // println(example.privateVar) // Error: privateVar is private
+    // println(example.protectedVar) // Error: protectedVar is protected
+    println(example.internalVar) // Accessible within the same module
+}
