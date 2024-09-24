b061aad9e164bc49a6b5f37ec018f8d4997f3dd3
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2024-09-24 22:30:09 +0530

    Inner Class

---
+++ src/50InnerClass.kt
@@ -0,0 +1,58 @@
+/*
+    Nested and Inner Classes in Kotlin -->
+
+    In Kotlin, you can define classes inside other classes.
+    These are called nested or inner classes.
+    The key difference between them is their access to the outer
+    class's properties and functions.
+
+    1. Nested Class ->
+
+    A nested class is a class defined inside another class.
+    By default, a nested class does not have access to the outer class's
+    properties or functions unless it is marked as inner.
+    It behaves like a static class in Java.
+
+    2. Inner Class ->
+
+    An inner class is a class that is defined inside another class and is
+    marked with the inner keyword.
+    An inner class has access to the properties and functions of its outer class.
+    You can refer to the outer class's instance using the keyword this@OuterClass.
+*/
+
+class OuterClass
+{
+
+    val outerProperty: String = "I am from the Outer Class"
+
+    // Nested class (doesn't have access to outer class properties)
+    class NestedClass
+    {
+        fun nestedMethod(): String
+        {
+            return "I am from the Nested Class"
+        }
+    }
+
+    // Inner class (has access to outer class properties)
+    inner class InnerClass
+    {
+        fun innerMethod(): String
+        {
+            return "I am from the Inner Class, accessing outer property: $outerProperty"
+        }
+    }
+}
+
+fun main()
+{
+    // Using the Nested Class
+    val nestedObject = OuterClass.NestedClass()
+    println(nestedObject.nestedMethod())
+
+    // Using the Inner Class
+    val outerObject = OuterClass()
+    val innerObject = outerObject.InnerClass()
+    println(innerObject.innerMethod())
+}
